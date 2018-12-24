ed97efaf10d5e35b451752cbc018dbf53d69e3c4
milawd <miladbonak@gmail.com>
2018-12-25 03:11:25 +0330

    server js code enhanced

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
+const bodyParser = require("body-parser");
 const routes = require("./routes");
-const defaultRoute = require('./utils/defaultRoute')
+const defaultRoute = require("./utils/defaultRoute");
+
+function onServerStartedSuccessfuly() {
+    console.log(`Server is up and running`);
+}
+
+function setupMiddlewares(app) {
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+}
+
 module.exports = app => {
+    setupMiddlewares(app);
+    app.listen(process.env.PORT || 3000, onServerStartedSuccessfuly);
+
     routes(app);
     app.use(defaultRoute);
 };
--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 const app = require("express")();
-const bodyParser = require("body-parser");
 const api = require("./api");
 const cronJobTaskRunner = require("./cron");
 
 global.isDebugMode = true; //for production or debug mode 
 global.__basedir = __dirname;
 
-const port = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 api(app);
 cronJobTaskRunner();
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
