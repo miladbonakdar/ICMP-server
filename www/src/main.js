988dc8886ca72f8367f2be8ae8d2a32b13a102de
omid naghipoor <naghipoor.omid@gmail,.com>
2019-01-11 03:16:25 +0330

    add loading component

--- www/src/main.js
+++ www/src/main.js
@@ -5,9 +5,12 @@ import App from "./App.vue";
 import {routes} from "./routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import Loading from "./components/Loading.vue";
 
 Vue.use(BootstrapVue);
 Vue.use(VueRouter);
+
+Vue.component("app-loading", Loading);
 
 const router = new VueRouter({
     routes
